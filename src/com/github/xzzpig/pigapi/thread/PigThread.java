8a7b92f47909f6f07e2dc8d9f1c334a6014aeed3
xzzpig <xzzpig@MgPsUbuntu>
2017-01-29 16:34:18 +0800

    添加PigThread

---
+++ src/com/github/xzzpig/pigapi/thread/PigThread.java
@@ -0,0 +1,34 @@
+package com.github.xzzpig.pigapi.thread;
+
+import com.github.xzzpig.pigapi.event.Event;
+import com.github.xzzpig.pigapi.event.EventAdapter;
+import com.github.xzzpig.pigapi.event.SimpleListener;
+
+public class PigThread extends Thread implements EventAdapter {
+	public Event e = new Event();
+
+	public PigThread(Runnable r) {
+		super(r);
+	}
+
+	public PigThread(Thread t) {
+		super(t::run);
+	}
+
+	@Override
+	public void run() {
+		this.callEvent(new PigThreadStartEvent(this));
+		super.run();
+		this.callEvent(new PigThreadFinishEvent(this));
+	}
+
+	@Override
+	public Event getEventInstance() {
+		return e;
+	}
+
+	public PigThread regListener(SimpleListener l) {
+		registListener(l);
+		return this;
+	}
+}
